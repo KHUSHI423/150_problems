864bf570ced308d0ece26cd927f8821649b4eb3d
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-14 11:23:36 +0530

    largest of 3 num

---
+++ sprint1/conditional/largestOf3.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+   a,b,c = map(int,input("Enter three numbers: ").split())
+   if a>=b and a>=c:
+      print(f"{a} is the largest number")
+   elif b>=a and b>=c:
+      print(f"{b} is the largest number")
+   else:
+      print(f"{c} is the largest number")
