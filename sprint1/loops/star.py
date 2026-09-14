e4e58b2fbea20f3d4408abc6df11d9d633917a87
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-14 14:03:26 +0530

    right aligned star triangle

---
+++ sprint1/loops/star.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    n = int(input("Enter the number: "))
+    for i in range(n):
+        for j in range(n - i - 1):
+            print(" ", end="")
+        for j in range(i + 1):
+            print("*", end="")
+        print()
