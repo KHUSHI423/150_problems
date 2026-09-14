463e0d898de87bb34c9be5455be0994fd59cb2b5
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-14 11:27:08 +0530

    grade calculator

---
+++ sprint1/conditional/grade.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    score = int(input("Enter the number: "))
+    if score >= 90:
+        print("Grade: A")
+    elif score >= 75:
+        print("Grade: B")
+    elif score >= 60:
+        print("Grade: C")
+    elif score >= 50:
+        print("Grade: D")
+    else:
+        print("Grade: F")
+        
