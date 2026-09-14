ffcfdd42f5984e9e868ae812117c702387bc1e26
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-14 14:15:28 +0530

    pyramid triangle

---
+++ sprint1/loops/pyramid.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    n = int(input("Enter the number: "))
+    for i in range(n):
+        for j in range(n - i - 1):
+            print(" ", end="")
+
+        for j in range(2*i+1):
+            print("*", end="")
+        print()
