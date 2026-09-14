075e0f8558fd01eb3235458d8a7155b4bd25b37c
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-14 10:58:32 +0530

    reverse of the number

---
+++ sprint1/numberTheory/reverse.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    n = int(input("Enter the number: "))
+    rev = 0
+    while n > 0:
+        digit = n % 10
+        rev = rev * 10 + digit
+        n //= 10
+    print("Reverse of the number is:", rev)
+    
