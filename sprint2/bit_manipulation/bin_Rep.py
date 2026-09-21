4fcd7b321ba15eeb0f7113f4f76b300e7cadb004
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-21 18:50:44 +0530

    binary representation

---
+++ sprint2/bit_manipulation/bin_Rep.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__" :
+    integerRep = int(input("enter the integer: "))
+    ans = ""
+    while integerRep :
+          ans =  str(integerRep%2) + ans
+          integerRep//=2
+    print(ans)
