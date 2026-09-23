07b34c4ea435007cb5abf867a4b29246bf6c1eca
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-23 21:46:33 +0530

    diamond problem

---
+++ sprint2/pattern/diamond.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n):
+    spaces = n - i +1
+    star = 2*i + 1
+    print(" "*spaces + "*"*star)
+for i in range(n-2,-1,-1):
+    spaces = n - i +1
+    star = 2*i + 1
+    print(" "*spaces + "*"*star)
