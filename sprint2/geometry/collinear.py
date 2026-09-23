437d9e034c6f09c87903cf0ae5174a5b62f156ae
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-23 21:10:28 +0530

    collinear

---
+++ sprint2/geometry/collinear.py
@@ -0,0 +1,13 @@
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+
+value = x1 * (y2 - y3) + \
+        x2 * (y3 - y1) + \
+        x3 * (y1 - y2)
+
+if value == 0:
+    print("Collinear")
+else:
+    print("Not Collinear")
+print("area: ",value/2)
