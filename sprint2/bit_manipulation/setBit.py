1ac7620cc756242638b0b86658773c17d6c3a88a
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-23 20:29:51 +0530

    updated method for finding the highest and lowest set position

--- sprint2/bit_manipulation/setBit.py
+++ sprint2/bit_manipulation/setBit.py
@@ -1,29 +1,7 @@
-if __name__ == "__main__":
+n = int(input("Enter the integer: "))
 
-    integerRep = int(input("Enter the integer: "))
+lowestSet = (n & -n).bit_length() - 1
+highestSet = n.bit_length() - 1
 
-    highestSet = lowestSet = 0
-
-    # Find lowest set bit
-    n = integerRep
-    i = 0
-
-    while n:
-        if n & 1 == 1:
-            lowestSet = i
-            break
-
-        i += 1
-        n >>= 1
-
-    # Find highest set bit
-    n = integerRep
-    i = 0
-
-    while n:
-        highestSet = i
-        i += 1
-        n >>= 1
-
-    print("Lowest set bit position:", lowestSet)
-    print("Highest set bit position:", highestSet)
+print("Lowest set bit:", lowestSet)
+print("Highest set bit:", highestSet)
