a849f490df682592a486e967cd8a1de510721f37
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-24 07:25:20 +0530

    hollow pyramid

---
+++ sprint2/pattern/hollowM.py
@@ -0,0 +1,11 @@
+m, n = map(int, input().split())
+
+for i in range(m):
+    for j in range(n):
+
+        if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print()
