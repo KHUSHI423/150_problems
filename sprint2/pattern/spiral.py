8504f2008078adf4d3d69f5c24a3fc19729b5565
KHUSHI423 <khushitirkey423@gmail.com>
2026-09-24 07:15:16 +0530

    spiral matrix

---
+++ sprint2/pattern/spiral.py
@@ -0,0 +1,33 @@
+n = int(input())
+num = 1
+top = 0
+bottom = n- 1
+right = n -1
+left = 0
+'''
+1 2 3
+8 9 4
+7 6 5
+'''
+mat = [[0]*n for _ in range(n)]
+while top<=bottom and left <= right:
+    for i in range(left,right+1):
+        mat[top][i] = num
+        num+=1
+    top+=1
+    for i in range(top,bottom+1):
+        mat[i][right] = num
+        num+=1
+    right-=1
+    if top <= bottom:
+        for i in range(right,left-1,-1):
+            mat[bottom][i] = num
+            num+=1
+        bottom-=1
+    if left<=right:
+        for i in range(bottom,top-1,-1):
+            mat[i][left] = num
+            num+=1
+        left+=1
+print(mat)
+
